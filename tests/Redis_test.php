a812585d697d815cd0985c0c78bb57846ab9e245
Joël Cox <joel@joelcox.nl>
2012-05-04 23:12:03 +0200

    Added thorough tests for _encode_request()

--- tests/Redis_test.php
+++ tests/Redis_test.php
@@ -4,7 +4,6 @@
  *
  * @see ../libraries/Redis.php
  */
-
 define('BASEPATH', TRUE);
 require_once('libraries/Redis.php');
 require_once('Stubs.php');
@@ -17,23 +16,87 @@ class RedisTest extends PHPUnit_Framework_TestCase {
 		$this->redis = new Redis();
 	}
 	
-	public function test_potocol()
+	/**
+	 * Test encode request
+	 *
+	 * Performs low-level tests on the encoding from
+	 * a command to the Redis protocol.
+	 */
+	public function test_encode_request()
 	{
-		$this->assertEquals($this->redis->command('PING'), 'PONG');
+		$method = new ReflectionMethod('Redis', '_encode_request');
+		$method->setAccessible(TRUE);
+		
+		// Individual command
+		$this->assertEquals(
+			$method->invoke(new Redis, 'PING'), 
+			"*1\r\n$4\r\nPING\r\n"
+		);
+		
+		// Command with a key and value, passed as a single argument
+		$this->assertEquals(
+			$method->invoke(new Redis, 'SET key value'), 
+			"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n"
+		);
+		
+		// Command with a key and value, passed as an array
+		$this->assertEquals(
+			$method->invoke(new Redis, 'SET', array('key' => 'value')), 
+			"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n"
+		);
+		
+		// Command with a multiple keys and values, passed as a string
+		$this->assertEquals(
+			$method->invoke(new Redis, 'HMSET key key1 value1 key2 value2'), 
+			"*6\r\n$5\r\nHMSET\r\n$3\r\nkey\r\n$4\r\nkey1\r\n$6\r\nvalue1\r\n$4\r\nkey2\r\n$6\r\nvalue2\r\n"
+		);
+		
+		// Command with a multiple keys and values, passed as an array
+		$this->assertEquals(
+			$method->invoke(new Redis, 'HMSET key', array('key1' => 'value1', 'key2' => 'value2')), 
+			"*6\r\n$5\r\nHMSET\r\n$3\r\nkey\r\n$4\r\nkey1\r\n$6\r\nvalue1\r\n$4\r\nkey2\r\n$6\r\nvalue2\r\n"
+		);
+		
+		// Command with a multiple keys and values, passed as an array, with spaces
+		$this->assertEquals(
+			$method->invoke(new Redis, 'HMSET key', array('key1' => 'value 1', 'key2' => 'value 2')), 
+			"*6\r\n$5\r\nHMSET\r\n$3\r\nkey\r\n$4\r\nkey1\r\n$7\r\nvalue 1\r\n$4\r\nkey2\r\n$7\r\nvalue 2\r\n"
+		);
+		
 	}
 	
+	/**
+	 * Test overloading
+	 *
+	 * Tests the overloading of commands through the 
+	 * __call magic method. The internals of the library
+	 * are treated as a blackbox.
+	 */
 	public function test_overloading()
 	{
-		$this->assertEquals($this->redis->hmset('myhash field1 Hello field2 World'), 'OK');
-		$this->assertEquals($this->redis->hget('myhash field1'), 'Hello');
-		$this->assertEquals($this->redis->del('myhash'), 1);
-	}
-	
-	public function test_misc()
-	{
-		$this->assertEquals($this->redis->set('key', 'value'), 'OK');
+		// Single command
+		$this->assertEquals($this->redis->ping(), 'PONG');
+		
+		// Arguments as a string
 		$this->assertEquals($this->redis->set('key value'), 'OK');
 		$this->assertEquals($this->redis->get('key'), 'value');
-	}	
+		$this->redis->del('key');
+
+		// Arguments as a seperate arguments
+		$this->assertEquals($this->redis->set('key', 'value'), 'OK');
+		$this->assertEquals($this->redis->get('key'), 'value');
+		$this->redis->del('key');
+		
+		// Multiple arguments as a string
+		$this->assertEquals($this->redis->hmset('key key1 value1 key2 value2'), 'OK');
+		$this->assertEquals($this->redis->hget('key key1'), 'value1');
+		$this->redis->del('key');
+
+		// Multiple arguments as an array
+		$this->assertEquals($this->redis->hmset('key', array('key1' => 'value1', 'key2' => 'value2')), 'OK');
+		$this->assertEquals($this->redis->hget('key', 'key1'), 'value1');
+		$this->redis->del('key');
+
+	}
 	
 }

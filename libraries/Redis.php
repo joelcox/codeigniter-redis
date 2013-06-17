6ea3aab1333bc656caebf717dbbe3bdaf4e03e5d
PanManAms <github@panman.nl>
2013-06-17 19:02:39 +0300

    Changed false to FALSE as styleguide mandates

--- libraries/Redis.php
+++ libraries/Redis.php
@@ -389,9 +389,9 @@ class CI_Redis {
 	 * of the server info instead of a nasty string.
 	 * @return 	array
 	 */
-	public function info($section=false)
+	public function info($section=FALSE)
 	{
-		if ($section!=false)
+		if ($section!=FALSE)
 		{
 			$response = $this->command('INFO '.$section);
 		}
